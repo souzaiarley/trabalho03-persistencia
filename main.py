c0a513ae073a5f7eef1c3bac4fb1c85d3738e655
Iarley Souza <souzaiarley20@gmail.com>
2026-01-14 21:13:11 -0300

    feat: implementa main inicial (somente rotas de home e alunos)

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 from fastapi import FastAPI
-from routes import home, alunos, autores, livros, emprestimos, estatisticas
+from contextlib import asynccontextmanager
+from routes import home, alunos
+#from routes import autores, livros, emprestimos, estatisticas
+from database import init_db, close_db
+from fastapi_pagination import add_pagination
 
-from models.aluno import Aluno
-from models.emprestimo import Emprestimo, EmprestimoWithLivro, EmprestimoWithAluno, EmprestimoFull
-from models.livro import Livro, LivroComEstatisticas
-from models.autor import Autor
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+    await close_db()
 
-Aluno.model_rebuild()
-Emprestimo.model_rebuild()
-EmprestimoWithLivro.model_rebuild()
-EmprestimoWithAluno.model_rebuild()
-EmprestimoFull.model_rebuild()
-Livro.model_rebuild()
-LivroComEstatisticas.model_rebuild()
-Autor.model_rebuild()
-
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(home.router)
 app.include_router(alunos.router)
-app.include_router(autores.router)
-app.include_router(livros.router)
-app.include_router(emprestimos.router)
-app.include_router(estatisticas.router)
+# app.include_router(autores.router)
+# app.include_router(livros.router)
+# app.include_router(emprestimos.router)
+# app.include_router(estatisticas.router)
+add_pagination(app)
