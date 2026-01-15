6b07fb16efd5456c2243a417ceab7d0513b89886
Iarley Souza <souzaiarley20@gmail.com>
2026-01-14 22:48:16 -0300

    refactor: remoção de __all__ no __init__

--- models/__init__.py
+++ models/__init__.py
@@ -6,5 +6,4 @@ from .livro import *
 Autor.model_rebuild()
 Livro.model_rebuild()
 Emprestimo.model_rebuild()
-
-__all__ = ["Aluno", "Autor", "Emprestimo", "Livro"]
+EmprestimoWithLivroOut.model_rebuild()
