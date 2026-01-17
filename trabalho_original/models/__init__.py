58f4cf44ee878f59779e3fea6e864b506ec75245
BrunoDev@777 <brugui2703@gmail.com>
2026-01-17 17:53:59 -0300

    Delete trabalho_original directory

--- trabalho_original/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .aluno import *
-from .autor import *
-from .emprestimo import *
-from .livro_autor_link import *
-from .livro import *
--- trabalho_original/models/livro_autor_link.py
+++
@@ -1,6 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-class LivroAutorLink(SQLModel, table=True):
-    """Tabela de ligação N:N entre livros e autores"""
-    livro_id: int = Field(foreign_key='livro.id', primary_key=True)
-    autor_id: int = Field(foreign_key='autor.id', primary_key=True)
